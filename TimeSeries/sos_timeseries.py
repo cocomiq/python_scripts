d12e0c8684c08adcec09812e7af950b31e5f9950
Semih Acikgoz/INL/TR <semih.acikgoz@avon.com>
2019-09-04 16:38:07 +0300

    Tutorial links is added

---
+++ TimeSeries/sos_timeseries.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+pd.date_range(start = '2018-12-22', periods = 13, freq = 'M')
+
+
+
+# https://machinelearningmastery.com/how-to-develop-deep-learning-models-for-univariate-time-series-forecasting/
+# https://towardsdatascience.com/predicting-sales-611cb5a252de
+# https://www.analyticsvidhya.com/blog/2018/09/multivariate-time-series-guide-forecasting-modeling-python-codes/
