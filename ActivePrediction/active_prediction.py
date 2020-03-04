e1f661fbc1da60231d13232fee100e1387c24119
Semih Acikgoz/INL/TR <semih.acikgoz@avon.com>
2020-03-04 11:18:35 +0300

    Campaign based runs

--- ActivePrediction/active_prediction.py
+++ ActivePrediction/active_prediction.py
@@ -5,13 +5,14 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import precision_score
 from sklearn.model_selection import train_test_split
+import pickle
 import matplotlib as plt
 
 # ==========================================================================
 # Load and prepare data
 # ==========================================================================
 
-df = pd.read_csv(r"C:\Users\acikgozs\Documents\active_prediction_v1.csv", sep = ";")
+df = pd.read_csv(r"C:\Users\acikgozs\Documents\active_prediction.csv", sep = ";")
 
 df.head()
 #df.drop(columns = ["CAMPAIGN", "ACCOUNT_NUMBER"], inplace = True)
@@ -39,7 +40,9 @@ eval_metric = ["auc","error"]
 
 #%time xgb_reg.fit(X_train, y_train, eval_metric = eval_metric, eval_set = eval_set, verbose = True)
 
-xgb_reg.fit(x_train, y_train)
+xgb_reg.load_model("active_xgb_save.model")
+#xgb_reg = pickle.load(open("active.model", 'rb'))
+#xgb_reg.fit(x_train, y_train)
 
 preds = xgb_reg.predict(x_test)
 
@@ -57,6 +60,13 @@ pd.DataFrame(preds.round())
 #y_test
 
 # ==========================================================================
+# Saving model
+# ==========================================================================
+
+#pickle.dump(xgb_reg, open("active.model", "wb"))
+xgb_reg.save_model("active_xgb_save.model")
+
+# ==========================================================================
 # Charts
 # ==========================================================================
 
@@ -65,3 +75,22 @@ plt.rcParams['figure.figsize'] = [50, 10]
 
 xgb.plot_importance(xgb_reg)
 plt.rcParams['figure.figsize'] = [5, 5]
+
+# ==========================================================================
+# Single campaign results
+# ==========================================================================
+
+dfp = pd.read_csv(r"C:\Users\acikgozs\Documents\active_prediction202002.csv", sep = ";")
+
+dfp.head()
+
+dfp.dropna()
+dfp = dfp[dfp["AGE"] > 0]
+dfp.reset_index()
+
+x = dfp.iloc[:,3:]
+#y = dfp.iloc[:,2:3]
+
+preds = xgb_reg.predict(x)
+
+pd.DataFrame(preds, dfp["ACCOUNT_NUMBER"]).to_csv("2020C3_pred.csv")
